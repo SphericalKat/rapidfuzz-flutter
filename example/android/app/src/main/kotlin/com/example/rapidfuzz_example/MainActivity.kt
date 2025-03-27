40121ae0bbc913a00562c50da923b5695dc69c9a
SphericalKat <amolele@gmail.com>
2025-03-27 12:38:56 +0900

    Initial commit
    Signed-off-by: SphericalKat <amolele@gmail.com>

---
+++ example/android/app/src/main/kotlin/com/example/rapidfuzz_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rapidfuzz_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ios/Classes/rapidfuzz.c
@@ -0,0 +1,3 @@
+// Relative import to be able to reuse the C sources.
+// See the comment in ../rapidfuzz.podspec for more information.
+#include "../../src/rapidfuzz.c"

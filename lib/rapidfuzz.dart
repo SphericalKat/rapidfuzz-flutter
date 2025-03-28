c908d643f9b3039009b279593acaba2e415adf7d
SphericalKat <amolele@gmail.com>
2025-03-28 12:51:10 +0900

    fix(wrapper): free native string after use
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- lib/rapidfuzz.dart
+++ lib/rapidfuzz.dart
@@ -10,25 +10,37 @@ import 'rapidfuzz_bindings_generated.dart';
 double ratio(String str1, String str2) {
   final str1Pointer = str1.toNativeUtf8();
   final str2Pointer = str2.toNativeUtf8();
-  return _bindings.ratio(str1Pointer.cast<Char>(), str2Pointer.cast<Char>());
+  final ratio = _bindings.ratio(str1Pointer.cast<Char>(), str2Pointer.cast<Char>());
+  malloc.free(str1Pointer);
+  malloc.free(str2Pointer);
+  return ratio;
 }
 
 double partialRatio(String str1, String str2) {
   final str1Pointer = str1.toNativeUtf8();
   final str2Pointer = str2.toNativeUtf8();
-  return _bindings.partial_ratio(str1Pointer.cast<Char>(), str2Pointer.cast<Char>());
+  final ratio = _bindings.partial_ratio(str1Pointer.cast<Char>(), str2Pointer.cast<Char>());
+  malloc.free(str1Pointer);
+  malloc.free(str2Pointer);
+  return ratio;
 }
 
 double tokenSortRatio(String str1, String str2) {
   final str1Pointer = str1.toNativeUtf8();
   final str2Pointer = str2.toNativeUtf8();
-  return _bindings.token_sort_ratio(str1Pointer.cast<Char>(), str2Pointer.cast<Char>());
+  final ratio = _bindings.token_sort_ratio(str1Pointer.cast<Char>(), str2Pointer.cast<Char>());
+  malloc.free(str1Pointer);
+  malloc.free(str2Pointer);
+  return ratio;
 }
 
 double tokenSetRatio(String str1, String str2) {
   final str1Pointer = str1.toNativeUtf8();
   final str2Pointer = str2.toNativeUtf8();
-  return _bindings.token_set_ratio(str1Pointer.cast<Char>(), str2Pointer.cast<Char>());
+  final ratio = _bindings.token_set_ratio(str1Pointer.cast<Char>(), str2Pointer.cast<Char>());
+  malloc.free(str1Pointer);
+  malloc.free(str2Pointer);
+  return ratio;
 }
 
 const String _libName = 'rapidfuzz';
